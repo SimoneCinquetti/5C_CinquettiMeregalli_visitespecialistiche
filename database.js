b1bcba8ddb05f0bfbea6d40b9867babf9c79cb14
Simone Cinquetti <cinquettisimone@itis-molinari.eu>
2025-02-20 16:24:12 +0100

    small database pathing fix

--- database.js
+++ database.js
@@ -1,6 +1,6 @@
 const mysql = require('mysql2');
 const fs = require('fs');
-const conf = JSON.parse(fs.readFileSync('./public/conf.json')).database;
+const conf = JSON.parse(fs.readFileSync('./conf.json'));
 conf.ssl.ca = fs.readFileSync(__dirname + '/ca.pem');
 const connection = mysql.createConnection(conf);
 
